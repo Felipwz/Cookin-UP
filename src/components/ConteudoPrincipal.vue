ad02c60b17ef47f35c001db6e6de865fddeea84a
Felipe <luizfelipesiqueira83@gmail.com>
2025-10-25 12:07:15 -0300

    Add conditional rendering for ingredient list and empty state message

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -4,13 +4,21 @@
             <span class="subtitulo-lg sua-lista-texto">
                 Sua lista:
             </span>
-            <ul class="ingredientes-sua-lista">
+            <ul v-if="ingredientes.length" class="ingredientes-sua-lista" >
                 <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
                     {{ ingrediente }} 
                      
                 </li>
             </ul>
+
+            <p v-else class="paragrafo lista-vazia" >
+              <img src="../assets/imagens-main/icones/lista-vazia.svg" alt="Imagem lista fazia">
+              Sua lista está vazia, selecione ingredientes para iniciar.
+            </p>
         </section>
+
+        
+
        </main>
 </template>
 
