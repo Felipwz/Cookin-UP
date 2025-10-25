f8e68e27d5a021596dfc2b37d54cdc3ca6aa2b91
Felipe <luizfelipesiqueira83@gmail.com>
2025-10-25 11:40:53 -0300

    Projeto Alura - Vue / Subindo repo

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Banner/>
+  <ConteudoPrincipal/>
+  <Hooks/>
+</template>
+
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+export default{
+  components: {Banner, ConteudoPrincipal},
+
+  data() {
+    
+  },
+
+
+  
+}
+
+
+</script>
