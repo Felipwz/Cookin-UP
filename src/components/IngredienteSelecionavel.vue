f5b7029097a62a75502cade59612e85cceed5fe8
Felipe <luizfelipesiqueira83@gmail.com>
2025-11-02 23:17:21 -0300

    feat: update IngredienteSelecionavel component to improve accessibility and styling

--- src/components/IngredienteSelecionavel.vue
+++ src/components/IngredienteSelecionavel.vue
@@ -1,10 +1,12 @@
 <template>
-  <button class="ingrediente" v-on:click="selecionado = !selecionado">
+  <button class="ingrediente" v-on:click="selecionado = !selecionado"
+ :aria-pressed="selecionado">
     <Tag :texto="ingrediente" :ativa="selecionado" />
   </button>
 </template>
 
 <script lang="ts">
+import { setEmitFlags } from "typescript";
 import Tag from "./Tag.vue";
 
 export default {
@@ -21,3 +23,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.ingrediente{
+    cursor: pointer;
+}
+</style>
